96158cc19e73f70f0f7d714bef696c8a0015ac4e
Teodor G <grigor.teodor@gmail.com>
2020-03-17 21:03:30 +0000

    initialize project

---
+++ src/main/cpp/main.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+using namespace std;
+using namespace sf;
+
+#define WindowX 800
+#define WindowY 500
+
+Vector2f theGameWindow_currentDimensions(WindowX, WindowY); // NOLINT(cert-err58-cpp)
+Vector2f theGameWindow_perspectiveDimensions(WindowX, WindowY); // NOLINT(cert-err58-cpp)
+RenderWindow theGameWindow( // NOLINT(cert-err58-cpp)
+        VideoMode(theGameWindow_currentDimensions.x, theGameWindow_currentDimensions.y),
+        "Template");
+
+void init();
+
+void inputListener();
+
+void update(float seconds);
+
+void draw();
+
+int main()
+{
+    Clock clockTime;
+    Time speed;
+
+    theGameWindow.setFramerateLimit(60);
+    init();
+
+    while (theGameWindow.isOpen())
+    {
+        inputListener();
+
+        speed = clockTime.restart();
+        update(speed.asSeconds());
+
+        theGameWindow.clear();
+
+        draw();
+
+        theGameWindow.display();
+    }
+
+    return 0;
+}
+
+void init()
+{
+    Event event{};
+
+    while (theGameWindow.pollEvent(event))
+    {
+        if (event.key.code == Keyboard::Escape || event.type == Event::Closed)
+        {
+            theGameWindow.close();
+        }
+    }
+
+}
+
+void inputListener()
+{
+
+}
+
+void update(float seconds)
+{
+
+}
+
+void draw()
+{
+
+}
